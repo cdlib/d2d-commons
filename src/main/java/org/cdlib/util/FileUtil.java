86ad17e9471d9546ca572d24cfc3a479a46a1067
jferrie <joe.ferrie@ucop.edu>
2020-01-10 15:13:40 -0800

    refactor FileUtil

--- src/main/java/org/cdlib/util/FileUtil.java
+++ src/main/java/org/cdlib/util/FileUtil.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.core.io.ClassPathResource;
@@ -13,12 +14,14 @@ import org.springframework.core.io.ClassPathResource;
  * 
  * Throws IllegalArgumentException in lieu of IOException, assuming that the problem is almost
  * always a bad path.
+ * 
+ * These utilities are largely based on strings, and will no doubt fail when reading a truly huge file.
  *
  */
 public class FileUtil {
   
   private FileUtil() {}
-
+  
   public static String read(String pathOnClasspath) {
     try {
       return readFromClasspath(pathOnClasspath, false);
@@ -26,7 +29,7 @@ public class FileUtil {
       throw new IllegalArgumentException("Unable to read file on classpath" + pathOnClasspath, e);
     }
   }
-
+  
   public static String readStripEOL(String pathOnClasspath) {
     try {
       return readFromClasspath(pathOnClasspath, true);
@@ -35,7 +38,28 @@ public class FileUtil {
     }
   }
 
-  public static void write(Path filePath, String content) {
+  public static void writeJsonToClasspath(String pathOnClasspath, Object pojo) {
+    writeToClasspath(pathOnClasspath, JSON.serialize(pojo));
+  }
+
+  public static void writeJsonToFilePath(String filePath, Object pojo) {
+    writeToFilepath(filePath, JSON.serialize(pojo));
+  }
+
+  public static void writeJsonToPath(Path filePath, Object pojo) {
+    writeToFilepath(filePath, JSON.serialize(pojo));
+  }
+  
+  public static void writeToClasspath(String pathOnClasspath, String content) {
+    try {
+      File file = new ClassPathResource(pathOnClasspath).getFile();
+      writeToFilepath(file.toPath(), content);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Class path " + pathOnClasspath + " is not writable.", e);
+    }
+  }
+
+  public static void writeToFilepath(Path filePath, String content) {
     if (!Files.isWritable(filePath)) {
       throw new IllegalArgumentException("File path " + filePath + " is not writable.");
     }
@@ -45,24 +69,11 @@ public class FileUtil {
       throw new IllegalArgumentException("File path " + filePath + " is not writable.", e);
     }
   }
-
-  public static void write(String pathOnClasspath, String content) {
-    try {
-      File file = new ClassPathResource(pathOnClasspath).getFile();
-      write(file.toPath(), content);
-    } catch (IOException e) {
-      throw new IllegalArgumentException("Class path " + pathOnClasspath + " is not writable.", e);
-    }
+  
+  public static void writeToFilepath(String path, String content) {
+    writeToFilepath(Paths.get(path), content);
   }
   
-  public static void writeJsonToClasspath(String pathOnClasspath, Object pojo) {
-    write(pathOnClasspath, JSON.serialize(pojo));
-  }
-  
-  public static void writeJsonToPath(Path filePath, Object pojo) {
-    write(filePath, JSON.serialize(pojo));
-  }
-
   private static String buildString(List<String> inputList) {
     StringBuilder builder = new StringBuilder();
     for (String s : inputList) {
@@ -70,7 +81,7 @@ public class FileUtil {
     }
     return builder.toString();
   }
-
+  
   private static String buildStringStripEOL(List<String> inputList) {
     StringBuilder builder = new StringBuilder();
     for (String s : inputList) {
