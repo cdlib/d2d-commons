1bd5b6831543553c505e953a66781413c2b014cd
Noahw135-CDL <noah.white@ucop.edu>
2020-08-18 13:19:58 -0700

    Restored issn equality

--- src/main/java/org/cdlib/domain/objects/bib/ISSN.java
+++ src/main/java/org/cdlib/domain/objects/bib/ISSN.java
@@ -64,5 +64,36 @@ public class ISSN implements Identifier {
     return JSON.serialize(this);
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((alternateValues == null) ? 0 : alternateValues.hashCode());
+    result = prime * result + ((value == null) ? 0 : value.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ISSN other = (ISSN) obj;
+    if (alternateValues == null) {
+      if (other.alternateValues != null)
+        return false;
+    } else if (!alternateValues.equals(other.alternateValues))
+      return false;
+    if (value == null) {
+      if (other.value != null)
+        return false;
+    } else if (!value.equals(other.value))
+      return false;
+    return true;
+  }
+
   
 }
